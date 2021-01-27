1ee2a4897fd3a96534e8c7d2309e7c4645fa658a
shbmjgdle <shbmjgdle@hotmail.com>
2021-01-27 23:36:42 +0530

    Front End Added

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
 
-
+BASE_CRAIGSLIST_URL = 'https://pune.craigslist.org/search/bbb?s=120'
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    final_url = BASE_CRAIGSLIST_URL
+    response = requests.get('https://pune.craigslist.org/search/bbb?query=cafe')
+    data = response.text
+    print(data)
+    stuff_for_frontend = {
+        'search': search,
+    }
+
+    return render(request,'my_app/new_search.html',stuff_for_frontend)
